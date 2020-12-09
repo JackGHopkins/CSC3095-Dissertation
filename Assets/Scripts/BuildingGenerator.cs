8947d48f18f24c91314f68dcc7847dfb66abf199
JackGHopkins <jackghopkins@gmail.com>
2020-12-09 18:11:01 +0000

    First Push

---
+++ Assets/Scripts/BuildingGenerator.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class BuildingGenerator : MonoBehaviour
+{
+    public int minPieces = 1;
+    public int maxPieces = 5;
+    public GameObject[] baseParts;
+    public GameObject[] middleParts;
+    public GameObject[] topParts;
+
+    public uint textureGridStepSizeX = 4;
+    public uint textureGridStepSizeY = 4;
+    public uint imgWidth = 512;
+    public uint imgHeight = 512;
+    [SerializeField] Texture2D texture;
+
+    private bool positionValid;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Build(); 
+    }
+
+    void Build()
+    {
+        int targetPieces = Random.Range(minPieces, maxPieces);
+
+        for (int i = minPieces - 1; i < maxPieces; i++)
+        {
+            SpawnBuilding(baseParts);
+        }
+    }
+
+    void SpawnBuilding(GameObject[] buildingArray)
+    {
+        Transform randomTransform = buildingArray[Random.Range(0, buildingArray.Length - 1)].transform;
+        GameObject clone = Instantiate(randomTransform.gameObject, GetSpawnPostion(), Quaternion.identity) as GameObject;
+        Mesh cloneMesh = clone.GetComponentInChildren<MeshFilter>().mesh;
+        Bounds bounds = cloneMesh.bounds;
+    }
+
+    Vector3 GetSpawnPostion()
+    {
+        float scaleRX = this.transform.localScale.x / imgWidth;
+        float scaleRZ = this.transform.localScale.z / imgHeight;
+
+        positionValid = false;
+        int x = 0;
+        int z = 0;
+        while (!positionValid) {
+            x = Mathf.FloorToInt(Random.Range(1, imgWidth));
+            z = Mathf.FloorToInt(Random.Range(1, imgHeight));
+
+            if (texture.GetPixel(x,z) != Color.black)
+            {
+                positionValid = true;
+            }
+
+        }
+        Vector3 position = new Vector3((x * scaleRX) - (this.transform.localScale.x / 2), 0, (z * scaleRZ) - (this.transform.localScale.x / 2));
+        return position;
+    }
+
+    bool BoundsCheck(int x, int y)
+    {
+        int sizeX;
+
+        return true;
+    }
+}
---
+++ Assets/Scripts/CubeGenerator.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CubeGenerator : MonoBehaviour
+{
+    public static CubeGenerator instance = null;
+
+    public uint textureGridStepSizeX = 4;
+    public uint textureGridStepSizeY = 4;
+    public uint imgWidth = 512;
+    public uint imgHeight = 512;
+
+    public GameObject vCube;
+    [SerializeField] float vHeightScale = 5.0f;
+    public RawImage rawImage;
+    public bool vGrid;
+
+    [SerializeField] Texture2D texture;
+
+    public object GeneratedObjectControl { get; private set; }
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (!instance)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        if (vGrid)
+        {
+            VisualizeGrid();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public float SampleStepped(int x, int y)
+    {
+        uint gridStepSizeX = imgWidth / textureGridStepSizeX;
+        uint gridStepSizeY = imgHeight / textureGridStepSizeY;
+
+        float sampledFloat = texture.GetPixel
+                   ((Mathf.FloorToInt(x * gridStepSizeX)), (Mathf.FloorToInt(y * gridStepSizeX))).grayscale;
+
+        return sampledFloat;
+    }
+
+    void VisualizeGrid()
+    {
+        GameObject vParent = new GameObject("VParent");
+        vParent.transform.SetParent(this.transform);
+
+        for (int x = 0; x < textureGridStepSizeX; x++)
+        {
+            for (int y = 0; y < textureGridStepSizeY; y++)
+            {
+                GameObject clone = Instantiate(vCube,
+                    new Vector3(x, SampleStepped(x, y) * vHeightScale, y)
+                    + transform.position, transform.rotation);
+
+                clone.transform.SetParent(vParent.transform);
+                instance.AddObject(clone);
+            }
+        }
+        //visualizationParent.transform.position =
+        //new Vector3(-perlinGridStepSizeX * .5f, -visualizationHeightScale * .5f, -perlinGridStepSizeY * .5f);
+    }
+
+    private void AddObject(GameObject clone)
+    {
+        throw new NotImplementedException();
+    }
+}
