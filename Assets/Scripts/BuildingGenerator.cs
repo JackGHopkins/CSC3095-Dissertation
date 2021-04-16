54f1f453685ee67973fee81eb9b4af3b2d4ac98d
JackGHopkins <jackghopkins@gmail.com>
2021-04-16 12:27:14 +0100

    Saving filling inside algorithm.

--- Assets/Scripts/BuildingGenerator.cs
+++ Assets/Scripts/BuildingGenerator.cs
@@ -3,6 +3,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
 
+struct Coordinate
+{
+    int x;
+    int y;
+    // To distinguish if road is on left or right of the pixel.
+    int shapeNo;
+    bool leftOfRoad;
+
+    public Coordinate(int x, int y, int shapeNo, bool leftOfRoad) 
+    {
+        this.x = x;
+        this.y = y;
+        this.shapeNo = shapeNo;
+        this.leftOfRoad = leftOfRoad;
+    }
+}
+
 public class BuildingGenerator : MonoBehaviour
 {
     public int minPieces = 1;
@@ -23,28 +40,46 @@ public class BuildingGenerator : MonoBehaviour
     private int buildingIndex;
     private int buildingNo;
 
+    [SerializeField] int buildingSpacing;
+    [SerializeField] Color32[] colors;
+
 
     // Start is called before the first frame update
     void Awake()
     {
-        Build(); 
+        Build();
     }
 
     void Build()
     {
-        int targetPieces = Random.Range(minPieces, maxPieces);
+        //int targetPieces = Random.Range(minPieces, maxPieces);
 
-        for (buildingIndex = minPieces - 1; buildingIndex < maxPieces; buildingIndex++)
+        //for (buildingIndex = minPieces - 1; buildingIndex < maxPieces; buildingIndex++)
+        //{
+        //    SpawnBuilding(baseParts);
+        //}
+
+        SpawnBuildingFloodFill();
+    }
+
+    void SpawnBuildingFloodFill()
+    {
+        StackBased sb = gameObject.AddComponent<StackBased>();
+        List<Vector2> perimeter = sb.FloodFill(texture, (int)imgWidth, (int)imgHeight, colors);
+
+        for(int i = 0; i < perimeter.Count;)
         {
-            SpawnBuilding(baseParts);
+            SpawnBuilding(baseParts, perimeter[i]);
+
+            i = i + buildingSpacing; 
         }
     }
 
-    void SpawnBuilding(GameObject[] buildingArray)
+    void SpawnBuilding(GameObject[] buildingArray, Vector2 position)
     {
         buildingNo = Random.Range(0, buildingArray.Length - 1);
         Transform randomTransform = buildingArray[buildingNo].transform;
-        GameObject clone = Instantiate(randomTransform.gameObject, GetSpawnPostion(), Quaternion.identity) as GameObject;      
+        GameObject clone = Instantiate(randomTransform.gameObject, new Vector3(position.x, 0, position.y), Quaternion.identity) as GameObject;      
         Mesh cloneMesh = clone.GetComponentInChildren<MeshFilter>().mesh;
         Bounds bounds = cloneMesh.bounds;
     }
@@ -112,5 +147,86 @@ public class BuildingGenerator : MonoBehaviour
         }
 
         return false;
+    }
+
+    // This function is trying to map out where the roads are.
+    public void ChartRoads()
+    {
+        bool onRoad;
+        int shapeCount = 0;
+
+        List<Coordinate> roadCoord = new List<Coordinate>();
+        Color[] pixels = texture.GetPixels(0,0, (int)imgWidth, (int)imgHeight);
+
+        if (pixels[0] == Color.black)
+        {
+            onRoad = true;
+        }
+        else
+        {
+            onRoad = false;
+            Coordinate coord = new Coordinate(0, 0, shapeCount, false);
+            roadCoord.Add(coord);
+        }
+        
+        for(int i = 1; i <= imgWidth * imgHeight; i++)
+        {
+           if(pixels[i] != Color.black && pixels[i + 1] == Color.black)
+           {
+                //roadCoord.Add(new Coordinate(i % (int)imgWidth, Mathf.FloorToInt(imgWidth / i), shapeCount, false));
+
+                /*
+                 * 1. Check adjaycent pixel. 3x3 with center being pixel[i]  
+                 * 2. If one is free then make index equal that spot. 
+                 * 3. 
+                 */
+
+                // Check pixel to the right
+                if (pixels[i + 1] != Color.black)
+                {
+
+                } 
+                // Check pixel above. Also, checks above is not null.
+                else if (i < (imgWidth * imgHeight) - imgWidth && pixels[i + imgWidth] != Color.black)
+                {
+
+                }
+                // Check pixel below. Also, checks below is not null.
+                else if (i > imgWidth && pixels[i + imgWidth] != Color.black)
+                {
+
+                }
+                // Check pixel top right corner.
+                else if (pixels[(i + imgWidth) + 1] != Color.black)
+                {
+
+                } 
+                // Check pixel top left corner.
+                else if (pixels[(i + imgWidth) - 1] != Color.black)
+                {
+
+                }
+                // Check pixel bottom right corner.
+                else if (pixels[(i - imgWidth) + 1] != Color.black)
+                {
+
+                }
+                // Check pixel bottom left corner.
+                else if (pixels[(i - imgWidth) - 1] != Color.black)
+                {
+
+                }
+
+            }
+        }
+    }
+
+    void checkSurroundingPixels(Color[] pixels, int i)
+    {
+        // Check pixel to the right
+        if(pixels[i + 1] == Color.black)
+        {
+
+        }
     }
 }
---
+++ Assets/Scripts/StackBased.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StackBased : MonoBehaviour
+{
+    bool[] pixelCheck;
+    Color32[] textureMip;
+
+    int textureWidth;
+    int textureHeight;
+    int currentMipPosition;
+
+    public List<Vector2> FloodFill(Texture2D texture, int textureHeight, int textureWidth, Color32[] colours)
+    {
+        List<Vector2> perimeter = new List<Vector2>();
+
+        currentMipPosition = 0;
+
+        textureMip = texture.GetPixels32();
+
+        this.textureWidth = textureWidth;
+        this.textureHeight = textureHeight;
+
+        foreach (Color32 currentColour in colours)
+        {
+
+            // Array to corrospond to whether or not that pixel in the Mip has been checked or not.
+            pixelCheck = new bool[textureMip.Length];
+
+            for (int pixelCount = 0; pixelCount < textureMip.Length; pixelCount++)
+            {
+                // Go to next loop if pixel has been checked.
+                if (pixelCheck[pixelCount])
+                    continue;
+
+                // Start flood fill if colours are the same.
+                if (Color.black == textureMip[pixelCount])
+                {
+                    //perimeter.Add(new Vector2(currentMipPosition % textureWidth, Mathf.Floor(currentMipPosition / textureHeight)));
+                    StackBased4Point(perimeter, currentColour, currentMipPosition);
+                }
+
+                // Pixel has now been checked.
+                if (!pixelCheck[pixelCount])
+                    pixelCheck[pixelCount] = true;
+
+                currentMipPosition++;
+            }
+        }
+        return perimeter;
+    }
+
+    // Recursive function
+    void StackBased4Point(List<Vector2> perimeter, Color32 colour, int currentMipPosition)
+    {
+        if (currentMipPosition >= textureMip.Length || currentMipPosition < 0)
+            return;
+
+        if (pixelCheck[currentMipPosition])
+            return;
+
+        // 1. If Node is not "Inside" then Return and add node to the list.
+        if (!textureMip[currentMipPosition].Equals(colour))
+        {
+            // Mip's array count goes from left to right, bottom to top. Hence, we find the modulo for the X coord and divide for the Y coord.
+            perimeter.Add(new Vector2(currentMipPosition % textureWidth, Mathf.Floor(currentMipPosition / textureHeight)));
+
+            Debug.Log("message");
+
+            // Set pixel to Checked.
+            pixelCheck[currentMipPosition] = true;
+            return;
+        }
+
+        // 2. Set the node
+        pixelCheck[currentMipPosition] = true;
+
+        // 3. Perform Flood-fill one step to the south of the node
+        StackBased4Point(perimeter, colour, currentMipPosition - textureWidth);
+
+        // 4. Perform Flood-fill one step to the north of the node
+        StackBased4Point(perimeter, colour, currentMipPosition + textureWidth);
+
+        // 5. Perform Flood-fill one step to the west of the node
+        StackBased4Point(perimeter, colour, currentMipPosition + 1);
+
+        // 6. Perform Flood-fill one step to the east of the node
+        StackBased4Point(perimeter, colour, currentMipPosition - 1);
+
+        // 7. Return
+        return;
+    }
+}
